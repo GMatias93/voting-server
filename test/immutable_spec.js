6d3bea29a8cfdec8cab9265effcc5544207baf7a
GMatias93 <gabriel.2m.deleon@gmail.com>
2016-01-19 19:50:36 -0800

    tests are working

--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -1,4 +1,4 @@
-import {expect} from 'chai';
+import { expect } from 'chai';
 
 describe('immutability', () => {
 
@@ -19,58 +19,60 @@ describe('immutability', () => {
 
   describe('a List', () => {
     function addMovie(currentState, movie) {
-			return currentState.push(movie);
-		}
+      return currentState.push(movie);
+    }
 
-		it('is immutability', () => {
-			let state = List.of('Trainspotting', '28 Days Later');
-			let nextState = addMovie(state, 'Sunshine');
+    it('is immutability', () => {
+      let state = List.of('Trainspotting', '28 Days Later');
+      let nextState = addMovie(state, 'Sunshine');
 
-			expect(nextState).to.equal(List.of(
-				'Trainspotting',
-				'28 Days Later',
-				'Sunshine'
-			));
-			expect(state).to.equal(List.of(
-				'Trainspotting',
-				'28 Days Later'
-			));
-		});
+      expect(nextState).to.equal(List.of(
+      'Trainspotting',
+      '28 Days Later',
+      'Sunshine'
+      ));
+      expect(state).to.equal(List.of(
+      'Trainspotting',
+      '28 Days Later'
+      ));
+    });
 
-	});
+  });
 
-	describe('a tree', () => {
-		function addMovie(currentState, movie) {
-			//Map.set(key,value): returns a new Map containing the new key,value pair
-			//replaces key if it's already present
-			return currentState.set(
-				'movies',
-				//Map.get(key): returns the value associated with the provided key
-				currentState.get('movies').push(movie)
-			);
-		}
+  describe('a tree', () => {
+    function addMovie(currentState, movie) {
 
-		it('is immutable', () => {
-			let state = Map({
-				movies: List.of('Trainspotting', '28 Days Later')
-			});
-			let nextState = addMovie(state, 'Sunshine');
+      //Map.set(key,value): returns a new Map containing the new key,value pair
+      //replaces key if it's already present
+      return currentState.set(
+      'movies',
 
-			expect(nextState).to.equal(Map({
-				movies: List.of(
-					'Trainspotting',
-					'28 Days Later',
-					'Sunshine'
-				)
-			}));
-			expect(state).to.equal(Map({
-				movies: List.of(
-					'Trainspotting',
-					'28 Days Later'
-				)
-			}));
-		});
+      //Map.get(key): returns the value associated with the provided key
+      currentState.get('movies').push(movie)
+      );
+    }
 
-	});
+    it('is immutable', () => {
+      let state = Map({
+        movies: List.of('Trainspotting', '28 Days Later'),
+      });
+      let nextState = addMovie(state, 'Sunshine');
+
+      expect(nextState).to.equal(Map({
+        movies: List.of(
+        'Trainspotting',
+        '28 Days Later',
+        'Sunshine'
+        ),
+      }));
+      expect(state).to.equal(Map({
+        movies: List.of(
+        'Trainspotting',
+        '28 Days Later'
+        ),
+      }));
+    });
+
+  });
 
 });
