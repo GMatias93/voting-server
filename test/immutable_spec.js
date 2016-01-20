8e981b555f3483155823101f4544110c74be6c70
GMatias93 <gabriel.2m.deleon@gmail.com>
2016-01-20 15:21:27 -0800

    test: added immutability test for Lists

--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -1,17 +1,41 @@
 import { expect } from 'chai';
+import { List } from 'immutable';
 
-describe('immutability', () => {
-  describe('a number', () => {
+describe('Immutability', () => {
+  describe('A Number', () => {
     function increment(currentState) {
       return currentState + 1;
     }
 
-    it('is immutable', () => {
+    it('Is immutable', () => {
       let state = 42;
       let nextState = increment(state);
 
       expect(nextState).to.equal(43);
       expect(state).to.equal(42);
+    });
+
+  });
+
+  describe('A List', () => {
+
+    function addMovie(currentState, movie) {
+      return currentState.push(movie);
+    }
+
+    it('Is immutable', () => {
+      let state = List.of('Transpotting', '28 Days Later');
+      let nextState = addMovie(state, 'Sunshine');
+
+      expect(nextState).to.equal(List.of(
+        'Transpotting',
+        '28 Days Later',
+        'Sunshine'
+      ));
+      expect(state).to.equal(List.of(
+        'Transpotting',
+        '28 Days Later'
+      ));
     });
 
   });
