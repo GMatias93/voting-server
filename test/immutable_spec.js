a92685a735945e76992cce6319e0c92e1e13e605
GMatias93 <gabriel.2m.deleon@gmail.com>
2016-01-20 15:48:43 -0800

    test: added test for immatability of a Map/Tree

--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { List } from 'immutable';
+import { List, Map } from 'immutable';
 
 describe('Immutability', () => {
   describe('A Number', () => {
@@ -36,6 +36,38 @@ describe('Immutability', () => {
         'Transpotting',
         '28 Days Later'
       ));
+    });
+
+  });
+
+  describe('A Tree', () => {
+
+    function addMovie(currentState, movie) {
+      return currentState.set(
+        'movies',
+        currentState.get('movies').push(movie)
+      );
+    }
+
+    it('Is immutable', () => {
+      let state = Map({
+        movies: List.of('Transpotting', '28 Days Later'),
+      });
+      let nextState = addMovie(state, 'Sunshine');
+
+      expect(nextState).to.equal(Map({
+        movies: List.of(
+          'Transpotting',
+          '28 Days Later',
+          'Sunshine'
+        ),
+      }));
+      expect(state).to.equal(Map({
+        movies: List.of(
+          'Transpotting',
+          '28 Days Later'
+        ),
+      }));
     });
 
   });
