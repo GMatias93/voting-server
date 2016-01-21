a1e063e650f387cd815da739eeeb654500a72041
GMatias93 <gabriel.2m.deleon@gmail.com>
2016-01-21 15:27:49 -0800

    feat: added continuous voting functionality

--- src/core.js
+++ src/core.js
@@ -9,8 +9,27 @@ export function setEntries(state, entries) {
   return state.set('entries', List(entries));
 }
 
+function getWinners(vote) {
+  if (!vote) {
+    return [];
+  }
+
+  const [a, b] = vote.get('pair');
+  const aVote = vote.getIn(['tally', a], 0);
+  const bVote = vote.getIn(['tally', b], 0);
+
+  if (aVote > bVote) {
+    return [a];
+  } else if (bVote > aVote) {
+    return [b];
+  } else {
+    return [a, b];
+  }
+}
+
 export function next(state) {
-  const entries = state.get('entries');
+  const entries = state.get('entries')
+                        .concat(getWinners(state.get('vote')));
   return state.merge({
     vote: Map({ pair: entries.take(2) }),
     entries: entries.skip(2),
